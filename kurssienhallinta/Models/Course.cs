568f587af9fbf96178705fb0237fa704a6ec8565
ExcavatorZ <excavatorz@hotmail.fi>
2025-11-04 10:34:50 +0200

    Luotu yhteys postgres tietokantaan, asennettu tarvittavat paketit, tehty konfigurointi, luotu malli kurssille ja testidataa.

---
+++ kurssienhallinta/Models/Course.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace kurssienhallinta.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set;  }
+        public string Description { get; set; }
+        public DateTime Day_of_start { get; set; }
+        public DateTime Day_of_end { get; set; }
+    }
+}
