40b01f286619754418e1bf2b3f17c9a914ff7048
TiiaSelin <tiiaselin92@gmail.com>
2025-11-19 14:22:05 +0200

    Update Teacher.cs

--- kurssienhallinta/Models/Teacher.cs
+++ kurssienhallinta/Models/Teacher.cs
@@ -4,7 +4,7 @@ namespace kurssienhallinta.Models
     public class Teacher
     {
         public int Id { get; set; }
-        public int Teacher_number { get; set; }
+        public string Teacher_code { get; set; }
         public string First_name { get; set; } = null!;
         public string Last_name { get; set; } = null!;
         public string Subject { get; set; } = null!;
