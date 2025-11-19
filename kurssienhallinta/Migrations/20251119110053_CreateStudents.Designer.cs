f45c18b28e680dc736ffcce0ba5489f8308682e0
ExcavatorZ <excavatorz@hotmail.fi>
2025-11-19 13:04:01 +0200

    Luotu taulu oppilaalle.

--- kurssienhallinta/Migrations/20251119110053_CreateStudents.Designer.cs
+++ kurssienhallinta/Migrations/20251119110053_CreateStudents.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using kurssienhallinta.Models;
@@ -11,9 +12,11 @@ using kurssienhallinta.Models;
 namespace kurssienhallinta.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251119110053_CreateStudents")]
+    partial class CreateStudents
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
