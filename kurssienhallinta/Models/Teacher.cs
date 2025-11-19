adca324efccb3ab6358328df1cf851ca64a4696f
ExcavatorZ <excavatorz@hotmail.fi>
2025-11-19 14:33:19 +0200

    Luotu migraatio opettajalle.

--- kurssienhallinta/Models/Teacher.cs
+++ kurssienhallinta/Models/Teacher.cs
@@ -4,9 +4,13 @@ namespace kurssienhallinta.Models
     public class Teacher
     {
         public int Id { get; set; }
+        [Required]
         public string Teacher_code { get; set; }
+        [Required]
         public string First_name { get; set; } = null!;
+        [Required]
         public string Last_name { get; set; } = null!;
+        [Required]
         public string Subject { get; set; } = null!;
     }
 
