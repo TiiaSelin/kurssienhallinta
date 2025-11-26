284d8ccbc8f97a46f2b3a462c64c42a96ea3ba17
ExcavatorZ <excavatorz@hotmail.fi>
2025-11-26 14:13:51 +0200

    Luotu testejä tilan lisäykselle ja tilan muokkaukselle.

--- Tests/RoomsControllerTest.cs
+++ Tests/RoomsControllerTest.cs
@@ -8,7 +8,7 @@ using Xunit;
 public class RoomsControllersTest
 {
     [Fact]
-    public void ListRooms()
+    public void Test_List_rooms()
     {
         // Initialize in-memory database and define mock data.
         var context = DbContextFactory.CreateInMemoryDbContext();
@@ -31,5 +31,95 @@ public class RoomsControllersTest
         Assert.Contains(model, room => room.Name == "Mush room");
         Assert.Contains(model, room => room.Capacity == 1);
         Assert.Contains(model, room => room.Room_code == "dk_0");
+    }
+    [Fact]
+    public void Test_Add_room_success()
+    {
+        var context = DbContextFactory.CreateInMemoryDbContext();
+        var logger = NullLogger<RoomsController>.Instance;
+        var controller = new RoomsController(context, logger);
+
+        var room = new Room
+        {
+            Name = "Sauna",
+            Capacity = 30,
+            Room_code = "sa_0"
+        };
+
+        var result = controller.Add_room(room);
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("List_rooms", redirect.ActionName);
+        Assert.Equal(1, context.Rooms.Count());
+    }
+    [Fact]
+    public void Test_Add_room_failure()
+    {
+        var context = DbContextFactory.CreateInMemoryDbContext();
+        var logger = NullLogger<RoomsController>.Instance;
+        var controller = new RoomsController(context, logger);
+
+        // Failed validation must be defined manually in unit test.
+        controller.ModelState.AddModelError("Name", "Required");
+
+        var room = new Room
+        {   
+            // No name, fails as a result.
+            Capacity = -1,
+            Room_code = "N/A"
+        };
+
+        var result = controller.Add_room(room);
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var returnedModel = Assert.IsType<Room>(viewResult.Model);
+        Assert.Equal(room.Room_code, returnedModel.Room_code);
+        Assert.Empty(context.Rooms);
+    }
+    [Fact]
+    public void Test_Edit_room_get()
+    {
+        var context = DbContextFactory.CreateInMemoryDbContext();
+        var logger = NullLogger<RoomsController>.Instance;
+        var controller = new RoomsController(context, logger);
+
+        context.Rooms.Add(new Room{Id = 1, Name = "Tyhjiö", Capacity = 0, Room_code = "ty_0"});
+        context.SaveChanges();
+
+        var result = controller.Edit_room(1);
+        var viewResult = Assert.IsType<ViewResult>(result);
+        var model = Assert.IsType<Room>(viewResult.Model);
+
+        Assert.Equal("Tyhjiö", model.Name);
+        Assert.Equal(0, model.Capacity);
+        Assert.Equal("ty_0", model.Room_code);
+    }
+    [Fact]
+    public void Test_Edit_room_post()
+    {
+        var context = DbContextFactory.CreateInMemoryDbContext();
+        var logger = NullLogger<RoomsController>.Instance;
+        var controller = new RoomsController(context, logger);
+
+        var room_to_edit = new Room
+        {
+            Id = 1,
+            Name = "Tyhjiö",
+            Room_code = "ty_0",
+            Capacity = 0
+        };
+
+        context.Rooms.Add(room_to_edit);
+        context.SaveChanges();
+
+        room_to_edit.Name = "Ei enää tyhjiö.";
+        room_to_edit.Capacity = 50;
+
+        var result = controller.Edit_room(room_to_edit);
+        var redirect = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("List_rooms", redirect.ActionName);
+
+        var room_in_db = context.Rooms.First(room => room.Id == 1);
+        Assert.Equal("Ei enää tyhjiö.", room_in_db.Name);
+        Assert.Equal(50, room_in_db.Capacity);
+        Assert.Equal("ty_0", room_in_db.Room_code);
     }
 }
