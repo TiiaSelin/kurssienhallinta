978e1fbf42e7ed1cb17885ab27b0fe33f3126114
ExcavatorZ <excavatorz@hotmail.fi>
2025-11-26 09:51:52 +0200

    Kokeillaan testejä.

---
+++ Tests/HomeControllerTest.cs
@@ -0,0 +1,16 @@
+﻿namespace kurssienhallinta.tests;
+using kurssienhallinta.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+
+public class ControllersTest
+{
+    [Fact]
+    public void HomeControllerTest()
+    {
+        var logger = NullLogger<HomeController>.Instance;
+        var controller = new HomeController(logger);
+        var result = controller.Index();
+        Assert.IsType<ViewResult>(result);
+    }
+}
